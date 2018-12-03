b696f507151e380bd43b978d6604363752a957b6
ruiduque <rmduque@icloud.com>
2018-12-03 10:27:37 +0000

    Hospital Analysis & best function code initial update

---
+++ US Hospital Analysis.R
@@ -0,0 +1,12 @@
+
+# Working directory on my local github lib
+setwd("/Users/ruisantos/Git/ProgAssignment3")
+
+# Ploting the 30-day mortality rates for hearth attacks
+outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+head(outcome)
+
+# Convert "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack" to numeric and show histogram
+outcome[,11] <- as.numeric(outcome[,11])
+hist(outcome[,11], main = names(outcome)[11])
+
---
+++ best.R
@@ -0,0 +1,17 @@
+# 
+# best function
+#
+# -- this function reads the outcome-of-care-measures.csv file and returns a character
+# -- vector with the name of the hospital that has the best (lowest) 30-day mortality
+# -- rate for the specified outcome in that state.
+
+best <- function(state, outcome) {
+        
+        ## Read outcome data
+        
+        ## Check that state and outcome are valid
+        
+        
+        ## Return hospital name in that state with lowest 30-day death rate
+        
+}
